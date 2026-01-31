f68765d2a178dad47fa4b0f81c607cc8a1304446
August Saint Freytag <me@augustfreytag.com>
2026-01-31 14:11:03 +0100

    Tune fog turbulence and shaping parameters

--- shaders/lib/overworld_fog.glsl
+++ shaders/lib/overworld_fog.glsl
@@ -5,8 +5,8 @@ uniform ivec2 eyeBrightness;
 #define FOG_USE_SHAPING 1
 #define FOG_USE_TURBULENCE 1
 
-const float FOG_TURBULENCE_MIX = 0.75;
-const float FOG_SHAPING_INTENSITY = 0.95;
+const float FOG_TURBULENCE_MIX = 0.8;
+const float FOG_SHAPING_INTENSITY = 0.75;
 
 // Utilities
 
